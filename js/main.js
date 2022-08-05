0a8c1fc59504d8eaba15f5b9e8140bb52b358fb1
krypton225 <k.mohamed.11.98@gmail.com>
2022-08-05 15:45:01 +0200

    ADD swiper lib & add testimonial section

--- js/main.js
+++ js/main.js
@@ -64,3 +64,22 @@ document.querySelectorAll('button[data-bs-toggle="tab"]').forEach((t, i) => {
 //     all.style.display = "block";
 //   });
 // }
+
+// * Swiper JS
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: ["-125%", 0, -800],
+      rotate: [0, 0, -90],
+    },
+    next: {
+      shadow: true,
+      translate: ["125%", 0, -800],
+      rotate: [0, 0, 90],
+    },
+  },
+});
